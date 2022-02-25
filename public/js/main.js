f0875f0076c586325bdb12da70df98b7d0a34345
Marius <marius@example.com>
2022-02-25 16:30:02 +0200

    gen posts ok

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 const URL = 'http://localhost:3000/posts';
 const postContainerEl = document.querySelector('.posts-grid');
-
+console.log('postContainerEl ===', postContainerEl);
 async function getPosts() {
   const resp = await fetch(URL);
   const jsData = await resp.json();
@@ -8,6 +8,19 @@ async function getPosts() {
   renderPosts(jsData, postContainerEl);
 }
 
-function renderPosts(postsArr, dest) {}
+function renderPosts(postsArr, dest) {
+  dest.innerHTML = '';
+  console.log('renderPosts ===');
+  postsArr.forEach(({ post_id, title, category, body }) => {
+    dest.innerHTML += `
+    <article class="one-post">
+        <h2 class="title">${title}</h2>
+        <p>category: <i>${category}</i></p>
+        <p class="post-intro">${body}</p>
+        <a href="singlePost.html?postId=${post_id}">View more</a>
+      </article>
+    `;
+  });
+}
 
 getPosts();
