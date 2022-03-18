000290d034e8130fa6e2e346e1f1c74827e5956f
Marius <marius@example.com>
2022-03-18 15:42:53 +0200

    renderSinglePost works

--- public/js/single.js
+++ public/js/single.js
@@ -2,10 +2,11 @@ const URL = 'http://localhost:3000/posts';
 console.log('single');
 
 const params = new URLSearchParams(window.location.search);
-
 const postId = params.get('postId');
 
-console.log(postId); // "sai"
+const h1El = document.getElementById('main-title');
+
+// console.log(postId); // "sai"
 
 async function getSinglePost(id) {
   // fetch get single post from back end
@@ -13,6 +14,19 @@ async function getSinglePost(id) {
   const [jsData] = await resp.json();
   console.log('getSinglePost ===', jsData);
   // get commentsByPosId
+  renderSinglePost(jsData, h1El);
 }
 
 getSinglePost(postId);
+
+function renderSinglePost(postObj, dest) {
+  const postHtml = `
+  <article class="one-post">
+    <h2 class="title">${postObj.title}</h2>
+    <p><i>${postObj.category}</i></p>
+    <p class="post-intro">${postObj.body}</p>
+    <a href="/public">Go back</a>
+  </article>
+  `;
+  dest.insertAdjacentHTML('afterend', postHtml);
+}
